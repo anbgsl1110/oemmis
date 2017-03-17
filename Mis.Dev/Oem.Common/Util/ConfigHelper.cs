5a623385ff7a4c9d1ebf2dca1fc2c8d94461d0c2
aliang <anbgsl1110@gmail.com>
2017-03-17 15:11:44 +0800

    增加读取appsettings.json配置文件的操作类

---
+++ Mis.Dev/Oem.Common/Util/ConfigHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace Oem.Common.Util
+{
+    /// <summary>
+    /// appsettings.json操作类
+    /// </summary>
+    public sealed class ConfigHelper
+    {
+        /// <summary>
+        /// 得到AppSettings中的配置字符串信息
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static string GetConfigString(string key, string defaultValue = "")
+        {
+            DirectoryInfo directoryInfo = Directory.GetParent(AppContext.BaseDirectory);
+            string basePath = directoryInfo.Parent.Parent.FullName;
+            IConfigurationRoot o = new ConfigurationBuilder()
+                .AddJsonFile(basePath + "\\appsettings.json").Build();
+            if (o != null) defaultValue = o.GetConnectionString(key);
+            return defaultValue;
+        }
+
+    }
+}
--- Mis.Dev/Oem.Providers/DbFactory.cs
+++ Mis.Dev/Oem.Providers/DbFactory.cs
@@ -1,12 +1,12 @@
 ﻿using System.Data;
 using MySql.Data.MySqlClient;
+using Oem.Common.Util;
 
 namespace Oem.Providers
 {
     public static class DbFactory
     {
-        private static readonly string ConnectionString =
-            @"server=114.215.158.176;port=5002;database=oemmis_dev;uid=root;pwd=000000;SslMode=None";
+        private static readonly string ConnectionString = ConfigHelper.GetConfigString("OemMisConn");
         private static readonly IDbConnection DbConnection = new MySqlConnection(ConnectionString);
 
         public static IDbConnection GetNewConnection()
