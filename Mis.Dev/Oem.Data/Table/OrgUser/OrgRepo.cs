cb100c640782ba2c07c4d1dea3e0687e5521c2ac
aliang <anbgsl1110@gmail.com>
2017-04-05 16:24:51 +0800

    添加log和org数据库映射对象

---
+++ Mis.Dev/Oem.Data/Table/OrgUser/OrgRepo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Oem.Data.Table.OrgUser
+{
+    /// <summary>
+    /// 机构对象
+    /// </summary>
+    public class OrgRepo
+    {
+        /// <summary>
+        /// 机构Id
+        /// </summary>
+        public long Id { get; set; }
+        /// <summary>
+        /// 机构名称
+        /// </summary>
+        public string OrgName { get; set; }
+        /// <summary>
+        /// 添加用户Id
+        /// </summary>
+        public long UserId { get; set; }
+        /// <summary>
+        /// 添加时间
+        /// </summary>
+        public DateTime AddDate { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+    }
+}
