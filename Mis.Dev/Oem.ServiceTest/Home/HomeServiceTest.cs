7e800f0e890e99a09f9e84ca0ada0186b77831f5
aliang <anbgsl1110@gmail.com>
2017-04-06 20:11:01 +0800

    添加第一个对于Service的单元测试

---
+++ Mis.Dev/Oem.ServiceTest/Home/HomeServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Oem.IServices.Home;
+using Oem.Services.Home;
+using Xunit;
+
+namespace Oem.ServiceTest.Home
+{
+    /// <summary>
+    /// HomeService单元测试
+    /// </summary>
+    public class HomeServiceTest
+    {
+        //home服务对象
+        private readonly IHomeService _homeService = new HomeService();
+
+        /// <summary>
+        /// 获取页面时间和本地时间差单元测试
+        /// </summary>
+        [Fact]
+        public void GetTimeDvalue()
+        {
+            var result = _homeService.GetTimeDvalue(100);
+            Assert.Equal(result > 0,true);
+        }
+    }
+}
