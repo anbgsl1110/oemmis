ea702ac1a2e4dcb8ef8ec53060a802a04d7918b4
aliang <anbgsl1110@gmail.com>
2017-04-13 16:09:22 +0800

    Update DbFactory.cs

--- Mis.Rider早期版/Oem.Providers/DbFactory.cs
+++ Mis.Rider早期版/Oem.Providers/DbFactory.cs
@@ -6,7 +6,7 @@ namespace Oem.Providers
     public static class DbFactory
     {
         private static readonly string ConnectionString =
-            @"server=114.215.158.176;port=5002;database=oemmis_dev;uid=root;pwd=000000;SslMode=None";
+            @"server=114.215.158.176;port=5002;database=oemmis_dev;uid=oemmis_dev;pwd=000000;SslMode=None";
         private static readonly IDbConnection DbConnection = new MySqlConnection(ConnectionString);
 
         public static IDbConnection GetNewConnection()
